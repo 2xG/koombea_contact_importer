7dc3b19175e8cc16f18dfe97ac6bbb634812c99d
Grigoriy Geveyler <mep3octb@gmail.com>
2021-07-07 15:24:49 -0500

    improve errors list view

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -6,4 +6,12 @@ module ContactsHelper
   def format_dob(dob)
     dob&.strftime '%Y %B %e'
   end
+
+  def error_list(contact)
+    content_tag :ol, class: 'list-group list-group-numbered' do
+      contact.error_list.split("\n").map do |error|
+        content_tag(:li, error, class: 'list-group-item')
+      end.reduce(&:+)
+    end
+  end
 end
